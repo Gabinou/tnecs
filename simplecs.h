06ab2150d25f86570a31f65e24719a12ba06665c
Gabriel Taillon <ga.taillon@gmail.com>
2021-04-10 00:52:47 +0900

    flecs has many useless abstractions.

--- simplecs.h
+++ simplecs.h
@@ -53,4 +53,8 @@ hmput(component_##name, entity_id, (name *)calloc(1, sizeof(name)));
 simplecs_entity_t simplecs_new_entity(struct Simplecs_World * in_world);
 simplecs_entity_t simplecs_entity_destroy(struct Simplecs_World * in_world, simplecs_entity_t in_entity);
 
+#define SIMPLECS_REGISTER_SYSTEM(world, name_sys, when, ...) 
+#define SIMPLECS_COMPONENTS_LIST(entity_list, Position)
+
+
 #endif // SIMPLECS
