987e4888ddf51df5a68904474a68335d1e746f30
Gabriel Taillon <ga.taillon@gmail.com>
2021-04-11 00:15:20 +0900

    many things are BORKED.

--- makefile
+++ makefile
@@ -8,11 +8,7 @@ INCLUDE_ALL = -I. -I${DIR_INCLUDE}
 # FLAGS_ERROR := -Wall -pedantic-errors
 FLAGS_ERROR :=
 
-ifeq ($(OS), Windows_NT)
-	EXEC := test.exe
-else
-	EXEC := CodenameFireSaga_make.bin 
-endif 
+EXEC := test.exe
 
 # FLAGS_BUILD_TYPE = -O3 -DNDEBUG #Release
 # FLAGS_BUILD_TYPE = -O0 -g  #Debug
@@ -25,8 +21,10 @@ all: astyle $(EXEC) run
 SOURCES_SIMPLECS := simplecs.c
 SOURCES_TEST := test.c
 HEADERS := $(wildcard *.h)
-SOURCES_ALL := $(SOURCES_TEST) $(SOURCES_SIMPLECS) 
+SOURCES_ALL := $(SOURCES_SC_TIMER) $(SOURCES_TEST) $(SOURCES_SIMPLECS) 
 TARGETS_SIMPLECS := $(SOURCES_SIMPLECS:.c=.o)
+# SOURCES_SC_TIMER := sc_time.c
+# TARGETS_SC_TIMER := $(SOURCES_SC_TIMER:.c=.o)
 
 .PHONY : run
 run: $(EXEC); $(EXEC)
@@ -35,9 +33,10 @@ run: $(EXEC); $(EXEC)
 astyle: $(HEADERS) $(SOURCES_ALL); astyle --style=java --indent=spaces=4 --indent-switches --pad-oper --pad-comma --pad-header --unpad-paren  --align-pointer=middle --align-reference=middle --add-braces --add-one-line-braces --attach-return-type --convert-tabs --suffix=none *.h *.c
 
 $(TARGETS_SIMPLECS) : $(SOURCES_SIMPLECS) ; $(CC) $< -c -o $@ 
+# $(TARGETS_SC_TIMER) : $(SOURCES_SC_TIMER) ; $(CC) $< -c -o $@ 
 $(EXEC): $(SOURCES_TEST) $(TARGETS_SIMPLECS); $(CC) $< $(TARGETS_SIMPLECS) -o $@ $(CFLAGS)
 
 .PHONY: wclean
 wclean: ; del /q /s *.o *.a *.exe build\\*.txt
 .PHONY: clean
-clean: ; @echo "Cleaning Simplecs"; rm -frv $(TARGETS_SIMPLECS) $(EXEC)
+clean: ; @echo "Cleaning Simplecs" & rm -frv $(TARGETS_SIMPLECS) $(TARGETS_SC_TIMER) $(EXEC)
--- test.c
+++ test.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <assert.h>
+#include <time.h>
+
 #include "stb_ds.h"
 #define STB_DS_IMPLEMENTATION
 #include "simplecs.h"
-#include <assert.h>
 
-// TIMER. get_time() resolution: 0.1 [us] 
+// TIMER. get_time() resolution: 0.1 [us]
+#ifdef __TINYC__
+// very low resolution
+#define get_time() ((double)clock())/CLOCKS_PER_SEC*1e6
+// #define get_time() (double)(sc_time_ns() * 1e3)
+// #define get_time() (double)clock()/CLOCKS_PER_SEC*1e6
+#else
 #ifdef WIN32
 #include <windows.h>
 double get_time() {
@@ -13,17 +21,15 @@ double get_time() {
     QueryPerformanceFrequency(&f);
     return 1e6 * (double)t.QuadPart / (double)f.QuadPart;
 }
-
 #else
 #include <sys/time.h>
-#include <sys/resource.h>
-
 double get_time() { // [s]
     struct timeval t;
     struct timezone tzp;
     gettimeofday(&t, &tzp);
     return 1e6 * t.tv_sec + t.tv_usec;
 }
+#endif
 #endif
 
 
@@ -37,12 +43,20 @@ typedef struct Unit {
     uint32_t str;
 } Unit;
 
+typedef struct Position2 {
+    uint32_t x;
+    uint32_t y;
+} Position2;
+
+typedef struct Unit2 {
+    uint32_t hp;
+    uint32_t str;
+} Unit2;
+
 
 #define ITERATIONS 100000
 #define ITERATIONS_SMALL 1000
 simplecs_entity_t simplecs_entities[ITERATIONS];
-ecs_entity_t flecs_entities[ITERATIONS];
-
 
 // void Simplecs_SystemMove(simplecs_entity_t * entity_list, size_t entity_num) {
 //     Position *p = SIMPLECS_COMPONENTS_LIST(entity_list, Position);
@@ -58,55 +72,56 @@ ecs_entity_t flecs_entities[ITERATIONS];
 
 int main() {
     printf("Hello, World! I am testing Simplecs. \n\n");
+    // SIMPLECS_REGISTER_SYSTEM(world, pfunc, phase, Position, Unit);
     simplecs_entity_t * components_list;
     struct Position * temp_position;
     struct Unit * temp_unit;
 
     printf("simplecs_init\n");
     struct Simplecs_World * test_world = simplecs_init();
-    assert(component_tables[SIMPLECS_NULLENTITY] == NULL);
+    // assert(component_tables[SIMPLECS_NULLENTITY] == NULL);
     printf("\n");
 
     printf("Component registration\n");
     printf("Registering Position Component \n");
-    SIMPLECS_REGISTER_COMPONENT(Position);
+    SIMPLECS_REGISTER_COMPONENT(test_world, Position);
     assert(Component_Position_id == COMPONENT_ID_START);
     printf("Registering Position Unit \n");
-    SIMPLECS_REGISTER_COMPONENT(Unit);
+    SIMPLECS_REGISTER_COMPONENT(test_world, Unit);
     assert(Component_Unit_id == (COMPONENT_ID_START + 1));
     printf("\n");
 
 
     printf("Entity Creation/Destruction\n");
-    assert(next_entity_id == ENTITY_ID_START);
+    assert(test_world->next_entity_id == ENTITY_ID_START);
     printf("Making Silou Entity \n");
     simplecs_entity_t Silou = simplecs_new_entity(test_world);
     assert(Silou == ENTITY_ID_START);
-    assert(next_entity_id == (ENTITY_ID_START + 1));
+    assert(test_world->next_entity_id == (ENTITY_ID_START + 1));
     printf("Making Pirou Entity \n");
     simplecs_entity_t Pirou = simplecs_new_entity(test_world);
     assert(Pirou == (ENTITY_ID_START + 1));
-    assert(next_entity_id == (ENTITY_ID_START + 2));
+    assert(test_world->next_entity_id == (ENTITY_ID_START + 2));
     assert(Silou != Pirou);
     printf("\n");
 
     printf("Adding Components to Entities\n");
     SIMPLECS_ADD_COMPONENT(test_world, Position, Silou);
-    components_list = hmget(test_world, Silou);
+    components_list = hmget(test_world->entities_table, Silou);
     assert(arrlen(components_list) == 1);
     assert(components_list[0] == Component_Position_id);
     SIMPLECS_ADD_COMPONENT(test_world, Unit, Silou);
-    components_list = hmget(test_world, Silou);
+    components_list = hmget(test_world->entities_table, Silou);
     assert(arrlen(components_list) == 2);
     assert(components_list[0] == Component_Position_id);
     assert(components_list[1] == Component_Unit_id);
 
     SIMPLECS_ADD_COMPONENT(test_world, Position, Pirou);
-    components_list = hmget(test_world, Pirou);
+    components_list = hmget(test_world->entities_table, Pirou);
     assert(arrlen(components_list) == 1);
     assert(components_list[0] == Component_Position_id);
     SIMPLECS_ADD_COMPONENT(test_world, Unit, Pirou);
-    components_list = hmget(test_world, Pirou);
+    components_list = hmget(test_world->entities_table, Pirou);
     assert(arrlen(components_list) == 2);
     assert(components_list[0] == Component_Position_id);
     assert(components_list[1] == Component_Unit_id);
@@ -172,14 +187,14 @@ int main() {
     simplecs_entity_destroy(test_world, Pirou);
     Pirou = simplecs_new_entity(test_world);
     assert(Pirou == (ENTITY_ID_START + 1));
-    assert(next_entity_id == (ENTITY_ID_START + 2));
+    assert(test_world->next_entity_id == (ENTITY_ID_START + 2));
     assert(Silou != Pirou);
     SIMPLECS_ADD_COMPONENT(test_world, Position, Pirou);
-    components_list = hmget(test_world, Pirou);
+    components_list = hmget(test_world->entities_table, Pirou);
     assert(arrlen(components_list) == 1);
     assert(components_list[0] == Component_Position_id);
     SIMPLECS_ADD_COMPONENT(test_world, Unit, Pirou);
-    components_list = hmget(test_world, Pirou);
+    components_list = hmget(test_world->entities_table, Pirou);
     assert(arrlen(components_list) == 2);
     assert(components_list[0] == Component_Position_id);
     assert(components_list[1] == Component_Unit_id);
@@ -190,14 +205,14 @@ int main() {
     double t_1;
 
     t_0 = get_time();
-    struct Simplecs_World * simplecs_world= simplecs_init();
+    struct Simplecs_World * bench_world = simplecs_init();
     t_1 = get_time();
     printf("simplecs: World Creation time \n");
     printf("%.1f [us] \n", t_1 - t_0);
 
     t_0 = get_time();
-    SIMPLECS_REGISTER_COMPONENT(Position) 
-    SIMPLECS_REGISTER_COMPONENT(Unit) 
+    SIMPLECS_REGISTER_COMPONENT(bench_world, Position2)
+    SIMPLECS_REGISTER_COMPONENT(bench_world, Unit2)
     t_1 = get_time();
     printf("simplecs: Component Registration \n");
     printf("%.1f [us] \n", t_1 - t_0);
@@ -205,11 +220,8 @@ int main() {
     t_0 = get_time();
     simplecs_entity_t simplecs_temp_ent;
     for (size_t i = 0; i < ITERATIONS; i++) {
-        // printf("i %d\n", i);
-        simplecs_temp_ent = simplecs_new_entity(simplecs_world);
+        simplecs_temp_ent = simplecs_new_entity(bench_world);
         simplecs_entities[i] = simplecs_temp_ent;
-
-        // printf("simplecs_temp_ent %d\n", simplecs_temp_ent);
     }
     t_1 = get_time();
     printf("simplecs: Entity Creation time: %d iterations \n", ITERATIONS);
@@ -218,13 +230,14 @@ int main() {
 
     t_0 = get_time();
     for (size_t i = 0; i < ITERATIONS; i++) {
-        SIMPLECS_ADD_COMPONENT(simplecs_world, Position, simplecs_entities[i]);
-        SIMPLECS_ADD_COMPONENT(simplecs_world, Unit, simplecs_entities[i]);
+        SIMPLECS_ADD_COMPONENT(bench_world, Position2, simplecs_entities[i]);
+        SIMPLECS_ADD_COMPONENT(bench_world, Unit2, simplecs_entities[i]);
     }
     t_1 = get_time();
     printf("simplecs: Component adding time: %d iterations \n", ITERATIONS);
     printf("%.1f [us] \n", t_1 - t_0);
 
     printf("Simplecs Test End");
+    printf("clock() %lf \n", clock());
     return (0);
 }
