9f08d43a25aaecf5f3fd229c7e230ccb01949e66
Gabriel Taillon <ga.taillon@gmail.com>
2021-05-16 15:54:20 +0900

    need more tests for macros.

--- tnecs.h
+++ tnecs.h
@@ -3,20 +3,20 @@
 
 // Tiny C99 Entity-Component-System (ECS) library.
 
-// Originally created for use in a game I am developping using C99: https://gitlab.com/Gabinou/firesagamaker. Title pending. 
+// Originally created for use in a game I am developping using C99: https://gitlab.com/Gabinou/firesagamaker. Title pending.
 
 // ECSs are an alternative way to organize data and functions to Object-Oriented programming (OOP).
-// * OOP: Objects/Classes contain data and methods. 
-// Methods act on objects. 
-// Children classes inherit methods and data structure from parents. 
+// * OOP: Objects/Classes contain data and methods.
+// Methods act on objects.
+// Children classes inherit methods and data structure from parents.
 // * ECS: Components are purely data.
 // Any number of components can be attached to an entity.
-// Entities are acted upon by systems. 
+// Entities are acted upon by systems.
 
-// In tnecs, an entity is an ```uint64_t``` index. 
-// A component is user-defined ```struct```. 
+// In tnecs, an entity is an ```uint64_t``` index.
+// A component is user-defined ```struct```.
 // A system is a user-defined ```function```.
-// All live inside the ```world```. 
+// All live inside the ```world```.
 
 // The systems iterate exclusively over the entities that have exactly the user-defined set of components, in phases.
 // Phases are user-defined ```size_t``` and system execution order is first-come first-served by default.
