fd10f35745faf0482fb2c6b3dca014db6890f300
Gabriel Taillon <ga.taillon@protonmail.com>
2025-01-01 22:12:55 +0900

    [rm] clock

--- test.c
+++ test.c
@@ -1,3 +1,8 @@
+/* Clock is POSIX, not C99 */
+#ifndef _POSIX_C_SOURCE
+    #define _POSIX_C_SOURCE 199309L
+#endif /* _POSIX_C_SOURCE */
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdint.h>
@@ -7,6 +12,75 @@
 #include <string.h>
 
 #include "tnecs.h"
+
+
+/********************** 0.1 MICROSECOND RESOLUTION CLOCK **********************/
+//  Modified from: https://gist.github.com/ForeverZer0/0a4f80fc02b96e19380ebb7a3debbee5
+#if defined(__linux)
+#  define MICROSECOND_CLOCK
+#  define HAVE_POSIX_TIMER
+#  include <time.h>
+#  ifdef CLOCK_MONOTONIC
+#     define CLOCKID CLOCK_MONOTONIC
+#  else
+#     define CLOCKID CLOCK_REALTIME
+#  endif
+#elif defined(__APPLE__)
+#  define MICROSECOND_CLOCK
+#  define HAVE_MACH_TIMER
+#  include <mach/mach_time.h>
+#elif defined(_WIN32)
+#  define MICROSECOND_CLOCK
+#  define WIN32_LEAN_AND_MEAN
+#  include <windows.h>
+#endif
+
+/********************** 0.1 MICROSECOND RESOLUTION CLOCK **********************/
+uint64_t tnecs_get_ns() {
+    static uint64_t is_init = 0;
+    #if defined(__APPLE__)
+    static mach_timebase_info_data_t info;
+    if (0 == is_init) {
+        mach_timebase_info(&info);
+        is_init = 1;
+    }
+    uint64_t now;
+    now = mach_absolute_time();
+    now *= info.numer;
+    now /= info.denom;
+    return now;
+    #elif defined(__linux)
+    static struct timespec linux_rate;
+    if (0 == is_init) {
+        clock_getres(CLOCKID, &linux_rate);
+        is_init = 1;
+    }
+    uint64_t now;
+    struct timespec spec;
+    clock_gettime(CLOCKID, &spec);
+    now = spec.tv_sec * 1.0e9 + spec.tv_nsec;
+    return now;
+    #elif defined(_WIN32)
+    static LARGE_INTEGER win_frequency;
+    if (0 == is_init) {
+        QueryPerformanceFrequency(&win_frequency);
+        is_init = 1;
+    }
+    LARGE_INTEGER now;
+    QueryPerformanceCounter(&now);
+    return (uint64_t)((1e9 * now.QuadPart) / win_frequency.QuadPart);
+    #endif
+}
+#ifdef MICROSECOND_CLOCK
+double tnecs_get_us() {
+    return (tnecs_get_ns() / 1e3);
+}
+#else
+#  define FAILSAFE_CLOCK
+#  define tnecs_get_us() (((double)clock())/CLOCKS_PER_SEC*1e6) // [us]
+#  define tnecs_get_ns() (((double)clock())/CLOCKS_PER_SEC*1e9) // [ns]
+#endif
+
 
 // TO DO:
 //   -> tests for component_del
